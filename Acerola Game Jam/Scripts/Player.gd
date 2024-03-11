fa73d56ea317a883a380fb6a61b7559bc3d17fd1
CastorBeaver <103660905+CastorBeaver@users.noreply.github.com>
2024-03-11 23:46:27 +1100

    Add files via upload

--- Acerola Game Jam/Scripts/Player.gd
+++ Acerola Game Jam/Scripts/Player.gd
@@ -6,13 +6,6 @@ var run_speed = 350
 var jump_speed = -1000
 var gravity = 2500
 
-var color1 = Color(1,0,0,1)
-var color2 = Color(0,1,0,1)
-var color3 = Color(0,0,1,0)
-var color12 = Color(1,1,0,1)
-var color13 = Color(1,0,1,1)
-var color23 = Color(0,1,1,1)
-
 func get_input():
 	velocity.x = 0
 	var right = Input.is_action_pressed('ui_right')
